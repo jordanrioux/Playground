6a6c89c4065d43fb0fa01dce9a4f7fa613ff2f22
Gabriel <90921447+gabgaron@users.noreply.github.com>
2022-05-20 11:20:17 -0400

    ok

--- Math.CLI/Program.cs
+++ Math.CLI/Program.cs
@@ -5,7 +5,7 @@ BasicMath.AddOperation("bob", (a, b) => -b);
 
 Console.WriteLine(BasicMath.Calculate(2, "+", 5));
 
-Console.WriteLine(BasicMath.Calculate(1, "/", 2000));
+Console.WriteLine(BasicMath.Calculate(1, "/", 177));
 Console.WriteLine(BasicMath.Calculate(1, "/", 2));
 Console.WriteLine(BasicMath.Calculate(1, "/", 2));
 Console.WriteLine(BasicMath.Calculate(1, "/", 2));
