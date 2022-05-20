8a2636d342227daaeb56cf4cea635a4761c060ea
BenocxX <mathis.cote@outlook.com>
2022-05-20 11:39:54 -0400

    feat: commit 2

--- Generics.CLI/Program.cs
+++ Generics.CLI/Program.cs
@@ -15,6 +15,7 @@ var jsonString = @"{
 }";
 
 Console.WriteLine("Hello wolrd first commit - Mathis");
+Console.WriteLine("Hello wolrd second commit - Mathis");
 
 // JSON to POCO
 var album = JsonSerializer.Deserialize<Artist>(jsonString);
