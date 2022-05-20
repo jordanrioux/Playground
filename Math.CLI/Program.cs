df5eec1a6aafcbec973f29da54ccdc3e10fad126
BenocxX <mathis.cote@outlook.com>
2022-05-20 11:10:43 -0400

    chore: remove comments

--- Math.CLI/Program.cs
+++ Math.CLI/Program.cs
@@ -3,19 +3,6 @@
 BasicMath.AddOperation("%", (a, b) => a % b);
 BasicMath.AddOperation("bob", (a, b) => -b);
 
-
-
-// (a, b) => a % b
-
-// () => 5 
-// () => {
-//      return 5;
-// }
-// public int Example()
-// {
-//      return 5;
-// }
-
 Console.WriteLine(BasicMath.Calculate(2, "+", 5));
 Console.WriteLine(BasicMath.Calculate(4, "-", 5));
 Console.WriteLine(BasicMath.Calculate(6, "*", 5));
