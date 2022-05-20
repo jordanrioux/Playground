17111a4fdd1b30a8233c2515f6ff24d91bd4fb0a
Ludovick31 <83981459+Ludovick31@users.noreply.github.com>
2022-05-20 11:40:34 -0400

    Update Program.cs

--- Math.CLI/Program.cs
+++ Math.CLI/Program.cs
@@ -2,7 +2,6 @@
 Console.WriteLine(BasicMath.Calculate(1, "/", 2));
 
 BasicMath.AddOperation("%", (a, b) => a % b);
-BasicMath.AddOperation("bob", (a, b) => -a);
 
 Console.WriteLine(BasicMath.Calculate(2, "+", 5));
 
